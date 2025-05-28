4174b9170efecf6f8194f6b8fedcfcb4139d4d6c
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-05-28 22:39:50 +0700

    feat: add todo mounting effect and SweetAlert2 popup on add

--- src/layouts/TodoLayout.jsx
+++ src/layouts/TodoLayout.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
+import Swal from "sweetalert2";
 const TodoLayout = () => {
-  const [todos, setTodos] = useState(["Belajar React", "Belajar Vite", "Belajar Tailwind CSS"]);
+  const [todos, setTodos] = useState(["Belajar React"]);
+  const inputTodo = useRef(null);
+
+  const addTodo = () => {
+    const newTodo = inputTodo.current.value;
+    if (newTodo) {
+      setTodos([...todos, newTodo]);
+      inputTodo.current.value = "";
+      Swal.fire({
+        title: "Succes",
+        text: "Berhasil menambahkan Todo",
+        icon: "success",
+      });
+    }
+  };
 
   return (
     <>
-      <input style={{ padding: "20px" }} type="text" placeholder="Masukkan todo baru" />
+      <input style={{ padding: "20px" }} type="text" placeholder="Masukkan todo baru" ref={inputTodo} />
 
-      <button style={{ margin: "5px", padding: "20px" }}>Tambah</button>
+      <button onClick={addTodo} style={{ margin: "5px", padding: "20px" }}>
+        Tambah
+      </button>
 
       {todos.map((item, index) => (
         <ul>
